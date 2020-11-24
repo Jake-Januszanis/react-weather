96c4c0884ec7a9ec257f647c035cfd0793cc50f8
Jacob Januszanis <Jjanuszanis94@gmail.com>
2020-11-24 09:34:16 -0700

    First commit. No Css added yet.

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express(); 
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended:false});
+const fetch = require("node-fetch");
+
+
+const port = process.env.PORT
+const apiKey = process.env.API_KEY
+
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+    
+})
+app.get('/', (req, res) => {
+    res.send('Server is running')
+}) 
+let zipcode;
+
+app.post('/', urlencodedParser, (req, res) => {
+    zipcode = req.body.zipcode;
+    if(zipcode.length === 5) {
+        res.redirect('/current-forecast')
+    } else {
+        res.redirect('/error')
+    }
+})
+
+app.get('/current-forecast', (req, res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apiKey}&units=imperial`)
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
