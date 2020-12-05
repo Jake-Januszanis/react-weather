92c2e4fc670cebb7058f39d0e3b010e32c971eba
Jacob Januszanis <Jjanuszanis94@gmail.com>
2020-12-05 12:08:44 -0700

    another commit

---
+++ client/src/components/error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Error() {
+    return(
+        <h1>Ooops looks like something went wrong. Please try again.</h1>
+    )
+}
+
+export default Error;
---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    purge: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+    theme: {
+      screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+      },
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
