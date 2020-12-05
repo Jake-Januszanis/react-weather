fab7720eeee66f274ea8ab7dbafe92769200a7eb
Jacob Januszanis <Jjanuszanis94@gmail.com>
2020-12-05 11:09:10 -0700

    Commit again

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('*', (req, res) => {
 
 })
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
     // res.send("Server is running")
 }) 
 let zipcode;
